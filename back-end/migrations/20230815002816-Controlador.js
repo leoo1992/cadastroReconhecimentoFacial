d7994c6e06678e6615370fb8a57c06004dff4591
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-28 15:53:01 -0300

    make auto seeders, routers, controller & refactors

--- back-end/migrations/20230815002816-Controlador.js
+++ back-end/migrations/20230815002816-Controlador.js
@@ -1,23 +1,30 @@
 "use strict";
-
 /** @type {import('sequelize-cli').Migration} */
+const { DataTypes } = require('sequelize');
+const controladorSeeder = require('../seeders/controladorSeeder');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Controlador", {
-      id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      tokenAutorizacao: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-    });
+    //alimentada
+    await controladorSeeder(),
+      await queryInterface.createTable("Controlador", {
+        id: {
+          allowNull: false,
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        tokenAutorizacao: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      });
   },
 
   down: async (queryInterface, Sequelize) => {
+    console.log('INITIALIZE UNDO: Controlador');
+    await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
     await queryInterface.dropTable("Controlador");
+    console.log('COMPLETE UNDO: Controlador');
   },
 };
