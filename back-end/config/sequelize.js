cbd05de8aeaedba90d2556e3acc372469720c6a3
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-28 15:52:50 -0300

    make auto seeders, routers, controller & refactors

--- back-end/config/sequelize.js
+++ back-end/config/sequelize.js
@@ -1,9 +1,7 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Carregar variáveis de ambiente do .env
-
+require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
 });
-
 module.exports = sequelize;
