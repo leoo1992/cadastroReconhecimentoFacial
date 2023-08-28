c620ae4dd48fae9fd190af7e95b48d520b013676
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-28 15:52:58 -0300

    make auto seeders, routers, controller & refactors

--- back-end/migrations/20230811225634-Pessoa.js
+++ back-end/migrations/20230811225634-Pessoa.js
@@ -1,43 +1,49 @@
 "use strict";
-
 /** @type {import('sequelize-cli').Migration} */
 const { DataTypes } = require('sequelize');
+const seedPessoa = require('../seeders/pessoaSeeder');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Pessoa", {
-      id: {
-        allowNull: false,
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      nome: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      cpf: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      tipo: {
-        type: DataTypes.ENUM("0", "1", "2", "3"),
-        allowNull: false,
-        defaultValue: "0",
-      },
-      ativo: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-        allowNull: false,
-      },
-      dataNascimento: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
-    });
+    //alimentada
+    await seedPessoa(),
+      await queryInterface.createTable("Pessoa", {
+        id: {
+          allowNull: false,
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        nome: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        cpf: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        tipo: {
+          type: DataTypes.ENUM("0", "1", "2", "3"),
+          allowNull: false,
+          defaultValue: "0",
+        },
+        ativo: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: true,
+          allowNull: false,
+        },
+        dataNascimento: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+      });
   },
 
   down: async (queryInterface, Sequelize) => {
+    console.log('INITIALIZE UNDO: Pessoa');
+    await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
     await queryInterface.dropTable("Pessoa");
+    console.log('COMPLETE UNDO: Pessoa');
   },
 };
