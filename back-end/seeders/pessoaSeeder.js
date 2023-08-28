e08d1adcbfdc0d98dbbdef826712d189e9a4acb1
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-28 15:54:02 -0300

    make auto seeders, routers, controller & refactors

---
+++ back-end/seeders/pessoaSeeder.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/sequelize');
+const Pessoa = require('../models/Pessoa');
+
+const seedPessoa = async () => {
+    try {
+        await sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
+        await Pessoa.sync();
+
+        const pessoaData = [
+            {
+                nome: 'João da Silva',
+                cpf: '12345678901',
+                tipo: '1',
+                ativo: true,
+                dataNascimento: '1990-01-01',
+            },
+            {
+                nome: 'Maria Oliveira',
+                cpf: '98765432101',
+                tipo: '1',
+                ativo: true,
+                dataNascimento: '1985-05-15',
+            },
+        ];
+        await Pessoa.bulkCreate(pessoaData);
+        await sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+        console.log('Dados de Pessoa inseridos com sucesso.');
+    } catch (err) {
+        console.error('Erro ao inserir dados de Pessoa:', err);
+        console.log('Erro detalhado:', err);
+    }
+};
+
+module.exports = seedPessoa;
