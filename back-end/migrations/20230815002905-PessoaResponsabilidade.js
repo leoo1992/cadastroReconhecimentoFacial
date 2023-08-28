8cae2fb1fb57eb3439c778d952f6407d24303860
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-28 15:53:13 -0300

    make auto seeders, routers, controller & refactors

--- back-end/migrations/20230815002905-PessoaResponsabilidade.js
+++ back-end/migrations/20230815002905-PessoaResponsabilidade.js
@@ -1,19 +1,25 @@
 "use strict";
-
 /** @type {import('sequelize-cli').Migration} */
+const { DataTypes } = require('sequelize');
+const seedPessoaResponsabilidade = require('../seeders/pessoaResponsabilidadeSeeder');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("PessoaResponsabilidade", {
-      id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-    });
+    // alimentada
+    await seedPessoaResponsabilidade(),
+      await queryInterface.createTable("PessoaResponsabilidade", {
+        id: {
+          allowNull: false,
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+      });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("PessoaResponsabilidade");
+    console.log('INITIALIZE UNDO: PessoaResponsabilidade');
+    await queryInterface.sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
+    await queryInterface.dropTable("pessoaresponsabilidade");
+    console.log('COMPLETE UNDO: PessoaResponsabilidade');
   },
 };
