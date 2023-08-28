52bdcaba76f314c6e5965eec80379d3e869ea48c
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-28 15:53:31 -0300

    make auto seeders, routers, controller & refactors

--- back-end/models/PessoaResponsabilidade.js
+++ back-end/models/PessoaResponsabilidade.js
@@ -1,20 +1,21 @@
 "use strict";
-const sequelize = require('sequelize');
-
-const Pessoa = require("./Pessoa");
+const { DataTypes } = require('sequelize'),
+  sequelize = require('../config/sequelize'),
+  Pessoa = require("./Pessoa");
 
 const PessoaResponsabilidade = sequelize.define("PessoaResponsabilidade", {
   id: {
     allowNull: false,
-    type: sequelize.INTEGER,
+    type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true,
   },
+}, {
+  tableName: "pessoaresponsabilidade"
 });
 
 PessoaResponsabilidade.belongsTo(Pessoa, { foreignKey: "FK_ResponsavelId" });
 Pessoa.hasMany(PessoaResponsabilidade);
-
 PessoaResponsabilidade.belongsTo(Pessoa, { foreignKey: "FK_SubordinadoId" });
 Pessoa.hasMany(PessoaResponsabilidade);
 
