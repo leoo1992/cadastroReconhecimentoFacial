cb81bdf0a071611155e59df8ae0385712436f49d
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-28 15:53:38 -0300

    make auto seeders, routers, controller & refactors

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express'),
+  router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 module.exports = router;
