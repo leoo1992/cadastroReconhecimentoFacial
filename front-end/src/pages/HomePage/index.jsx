8b1c87160fb5152ed5b6ba48265a8f5e547dc897
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-08-29 18:23:00 -0300

    futuras alterações prontas

--- front-end/src/pages/HomePage/index.jsx
+++ front-end/src/pages/HomePage/index.jsx
@@ -8,9 +8,19 @@ const HomePage = () => {
       <div className="bg-fundo flex-container">
         <h3 className="p-0 m-0 text-start text-danger d-inline">Reconhecimento Facial</h3>
         <div className="btn-group">
+
+        <div className="justify-content-end">
+          /*
+          TODO: Rota relatorio
+          */
+            <Link to="/relatorio" className="btn btn-success fw-bold mr-1">
+              Relatório de Acessos
+            </Link>
+          </div>
+
           <div className="justify-content-end">
             <Link to="/cadastro" className="btn btn-danger fw-bold">
-              Cadastrar
+              Cadastro de Pessoas
             </Link>
           </div>
         </div>
