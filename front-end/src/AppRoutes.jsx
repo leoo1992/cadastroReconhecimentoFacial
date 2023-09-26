9567aafb0c7745e076f7660ecbe607a3ee8356be
Leonardo <santos-contato@hotmail.com.br>
2023-09-25 22:01:43 -0300

    add front private routes

--- front-end/src/AppRoutes.jsx
+++ front-end/src/AppRoutes.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 import HomePage from "./pages/HomePage";
 import Cadastro from "./pages/Cadastro";
-import Login from "./pages/LoginPage";
 import Listas from "./pages/Listas";
+import Login from "./pages/LoginPage";
+import CadastrarUser from "./pages/LoginPage/CadastroUser";
 import Atualiza from "./pages/Cadastro/Atualiza";
 import Cadastrados from "./pages/Listas/Cadastrados";
 
 const AppRoutes = () => {
+  // eslint-disable-next-line
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />}/>
-        <Route exact path="/home" element={<HomePage />} />
-        <Route exact path="/cadastro" element={<Cadastro />} />
-        <Route exact path="/listas" element={<Listas />} />
-        <Route exact path="/cadastrados" element={<Cadastrados />} />
-        <Route path="/atualiza/:id" element={<Atualiza />} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/cadastrousers" element={<CadastrarUser />} />
         <Route exact path="/reconhecimento" />
         <Route exact path="/logs" />
-        <Route exact path="/users" />
       </Routes>
+
+        <PrivateRoute
+          path="/home"
+          element={<HomePage />}
+          authenticated={authenticated}
+        />
+        <PrivateRoute
+          path="/cadastro"
+          element={<Cadastro />}
+          authenticated={authenticated}
+        />
+        <PrivateRoute
+          path="/listas"
+          element={<Listas />}
+          authenticated={authenticated}
+        />
+        <PrivateRoute
+          path="/cadastrados"
+          element={<Cadastrados />}
+          authenticated={authenticated}
+        />
+        <PrivateRoute
+          path="/atualiza/:id"
+          element={<Atualiza />}
+          authenticated={authenticated}
+        />
     </Router>
   );
 };
---
+++ front-end/src/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ element: Element, authenticated, ...rest }) => {
+  return authenticated ? (
+    <Element {...rest} />
+  ) : (
+    <Navigate to="/" replace={true} />
+  );
+};
+
+export default PrivateRoute;
