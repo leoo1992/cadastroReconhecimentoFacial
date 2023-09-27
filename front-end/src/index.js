640fbe8014d785e42c44e548a0379b026dfda3c1
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-09-27 16:20:06 -0300

    update react-dom for react-dom/client

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-toastify/dist/ReactToastify.css';
 
-const exibirAviso = (mensagem) => {
-  console.log(`Aviso: Alguma funcionalidade esta incapaz no momento!`);
+// const exibirAviso = (mensagem) => {
+//   console.log(`Aviso: Alguma funcionalidade esta incapaz no momento!`);
+// };
+// console.error = () => {
+//   exibirAviso();
+// };
+// console.warn = () => {
+//   exibirAviso();
+// };
+const root = document.getElementById('root');
+
+const render = () => {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 };
 
-console.error = () => {
-  exibirAviso();
-};
-console.warn = () => {
-  exibirAviso();
-};
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render();
 
 reportWebVitals();
