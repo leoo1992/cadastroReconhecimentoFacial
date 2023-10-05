b0954e6b7e395a2e88b7f49e88a8ca9a3cb1d54b
Leonardo <santos-contato@hotmail.com.br>
2023-10-05 20:31:44 -0300

    make the route logs in the my front

--- front-end/src/AppRoutes.jsx
+++ front-end/src/AppRoutes.jsx
@@ -8,6 +8,7 @@ import CadastrarUser from "./pages/LoginPage/CadastroUser";
 import Atualiza from "./pages/Cadastro/Atualiza";
 import Cadastrados from "./pages/Listas/Cadastrados";
 import Usuarios from "./pages/Listas/Usuarios";
+import Logs from "./pages/Listas/Logs";
 
 const AppRoutes = () => {
   const [authenticated, setAuthenticated] = useState(false);
@@ -32,6 +33,7 @@ const AppRoutes = () => {
 
         <Route path="/listas" element={authenticated ? <Listas /> : <Navigate to="/" />} />
         <Route path="/cadastrados" element={authenticated ? <Cadastrados /> : <Navigate to="/" />} />
+        <Route path="/logs" element={authenticated ? <Logs /> : <Navigate to="/" />} />
         <Route path="/atualiza/:id" element={authenticated ? <Atualiza /> : <Navigate to="/" />} />
         <Route path="/usuarios" element={authenticated ? <Usuarios /> : <Navigate to="/" />} />
 
