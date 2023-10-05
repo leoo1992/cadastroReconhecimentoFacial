30a2bcd3bf9854079862e1bf247deade79a12013
Leonardo <santos-contato@hotmail.com.br>
2023-10-04 21:50:58 -0300

    add test of route for include new user

---
+++ back-end/__tests__/user.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Testes para a rota de cadastro usuario", () => {
+
+  it("Deve retornar um 200 ao fazer cadastro", async () => {
+    const response = await request(app)
+      .post("/cadastrousuarios")
+      .send({ usuario: "abc@abc.abc", senha: "abc123456" });
+
+    expect(response.status).toBe(200);
+  });
+});
