4d5b7558c871aeb02e0ccf3e815f5692b0a5de4b
Leonardo <santos-contato@hotmail.com.br>
2023-10-04 21:48:42 -0300

    include tests to delete user not found

---
+++ back-end/__tests__/deleteuser.test.js
@@ -0,0 +1,15 @@
+
+const request = require("supertest");
+const app = require("../app");
+
+describe("Testes para a rota de exclusão de usuário", () => {
+  it("Deve retornar um status 404 ao tentar excluir um usuário que não existe", async () => {
+    const idNaoExistente = 123;
+
+    const response = await request(app)
+      .delete(`/deletaruser/${idNaoExistente}`)
+      .send();
+
+    expect(response.status).toBe(404);
+  });
+});
