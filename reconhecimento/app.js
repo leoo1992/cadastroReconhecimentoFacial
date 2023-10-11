60fe5d4564fd57e1c604420ce765b003ca036bbd
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-10-11 13:41:25 -0300

    Size limit, name check, accuracy boost.

--- reconhecimento/app.js
+++ reconhecimento/app.js
@@ -3,47 +3,33 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const port = 3002;
 const filePath = path.join(__dirname, 'RostosConhecidos.txt');
 const labels = [];
-const { exec } = require('child_process');
+
+app.use(express.json());
 app.use(cors());
-
-app.get('/obter-nomes', (req, res) => {
-    res.json(labels);
-});
-
 app.use(express.static('public'));
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.raw({ type: 'image/jpeg', limit: '10mb' }));
-
-app.post('/salvar-imagem', (req, res) => {
-    const nome = req.headers.nome;
-
-    if (!nome) {
-        return res.status(400).send('Nome não fornecido.');
-    }
-
-    const imageData = req.body;
-
-    const pastaDestino = path.join(__dirname, 'reconhecimento' ,'assets', 'lib', 'face-api', 'labels', nome);
-    if (!fs.existsSync(pastaDestino)) {
-        fs.mkdirSync(pastaDestino, { recursive: true });
-    }
-
-    const caminhoImagem = path.join(pastaDestino, '1.jpg');
-    fs.writeFileSync(caminhoImagem, imageData);
-
-    fs.appendFileSync(filePath, '\n' + nome);
-
-    res.status(200).send('Imagem e nome salvos com sucesso.');
-});
 
 app.listen(port, () => {
     console.log('Servidor rodando na porta ' + port);
 });
 
+app.get('/obter-nomes', (req, res) => {
+    res.json(labels);
+});
+
+app.use(bodyParser.raw({
+    type: 'image/jpeg',
+    limit: '5mb',
+    verify: (req, res, buf, encoding) => {
+        if (buf.length > 5 * 1024 * 1024) {
+            res.status(400).send('A imagem enviada é maior do que 5MB.');
+        }
+    }
+}));
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -59,3 +45,40 @@ fs.readFile(filePath, 'utf8', (err, data) => {
 
     console.log('Nomes carregados:', labels);
 });
+
+app.post('/salvar-imagem', (req, res) => {
+    const nome = req.headers.nome;
+
+    if (!nome) {
+        return res.status(400).send('Nome não fornecido.');
+    }
+
+    const imageData = req.body;
+
+    const pastaDestino = path.join(__dirname, 'assets', 'lib', 'face-api', 'labels', nome);
+    if (!fs.existsSync(pastaDestino)) {
+        fs.mkdirSync(pastaDestino, { recursive: true });
+    }
+
+    let contador = 1;
+    while (fs.existsSync(path.join(pastaDestino, `${contador}.jpg`)) && contador <= 5) {
+        contador++;
+    }
+
+    if (contador > 5) {
+        return res.status(400).send('A pasta está cheia (limite de 5 imagens).');
+    }
+
+    const caminhoImagem = path.join(pastaDestino, `${contador}.jpg`);
+    fs.writeFileSync(caminhoImagem, imageData);
+
+    if (!labels.includes(nome)) {
+        labels.push(nome);
+        fs.appendFileSync(filePath, '\n' + nome);
+    }
+
+    res.status(200).send('Imagem e nome salvos com sucesso.');
+});
+
+
+
