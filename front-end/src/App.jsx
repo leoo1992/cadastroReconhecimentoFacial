18065299a5423eec96a6181f90f1fa30289b4c0b
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-10-13 18:01:40 -0300

    remove unused imports

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import AppRoutes from "./AppRoutes";
 import "bootstrap/dist/css/bootstrap.css";
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
-    <AppRoutes />
+      <AppRoutes />
     </div>
   );
 }
