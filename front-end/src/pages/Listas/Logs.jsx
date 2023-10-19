a3f43277f7da93006560507d91d8e4c37c3e37f7
Leonardo <santos-contato@hotmail.com.br>
2023-10-18 22:10:10 -0300

    add chart log

--- front-end/src/pages/Listas/Logs.jsx
+++ front-end/src/pages/Listas/Logs.jsx
@@ -9,11 +9,13 @@ import Tooltip from 'react-bootstrap/Tooltip';
 import { SearchField } from '@aws-amplify/ui-react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilePdf, faFileExcel } from "@fortawesome/free-solid-svg-icons";
+import { faFilePdf, faFileExcel, faChartBar } from "@fortawesome/free-solid-svg-icons";
 import { toast, ToastContainer } from 'react-toastify';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 import * as XLSX from 'xlsx';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 const Logs = () => {
   const [theme, setTheme] = useState("dark");
@@ -23,6 +25,23 @@ const Logs = () => {
   const [totalRows, setTotalRows] = useState(0);
   const [page, setPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState('');
+  const [showChart, setShowChart] = useState(false);
+  const paginationOcultar = document.getElementsByClassName('rdt_Pagination').get(0);
+  paginationOcultar.classList.add()
+
+
+  const toggleChartVisibility = () => {
+    setShowChart(!showChart);
+  };
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Legend
+  );
+
 
   const exportToPDF = async () => {
     try {
@@ -119,6 +138,9 @@ const Logs = () => {
   );
   const printButtonTooltipExcel = (
     <Tooltip id="add-button-tooltip">Imprimir Excel</Tooltip>
+  );
+  const geraGraficoButtonTooltip = (
+    <Tooltip id="add-button-tooltip">Mostrar Grafico</Tooltip>
   );
 
   const fetchUsers = useCallback(async (page, perPage, searchQuery) => {
@@ -276,6 +298,46 @@ const Logs = () => {
     setSearchQuery(newValue);
   };
 
+  const chartData = {
+    labels: formattedData.map((data) => data.pessoaNome),
+    datasets: [
+      {
+        label: 'Hora de Entrada',
+        data: formattedData.map((data) => data.hora),
+        color: theme === "dark" ? 'cyan' : 'darkblue',
+        labels: {
+          color: theme === "dark" ? 'cyan' : 'darkblue',
+        },
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: 'Logs',
+        color: theme === "dark" ? 'cyan' : 'darkblue',
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          color: theme === "dark" ? 'gray' : 'black',
+        },
+      },
+      y: {
+        grid: {
+          color: theme === "dark" ? 'gray' : 'black',
+        },
+      },
+    },
+  };
+
   return (
     <>
       <ToastContainer
@@ -290,83 +352,98 @@ const Logs = () => {
         pauseOnHover={false}
         theme='dark'
       />
-      {loading ? (
-        <div className='container-fluid text-center m-0 p-0 d-flex flex-column justify-content-center align-items-center align-self-center vh-100 vw-100 bg-fundo'>
-          <Triangle
-            color='cyan'
-            className='vh-100 vw-100 p-0 m-0'
-            ariaLabel="triangle-loading"
-            visible={true}
-          />
-          <h1 className='text-center text-info mt-3 pt-3'>{customText.loading}</h1>
-        </div>
-      ) : (
-        <>
-          <div className="top-0 text-end bg-fundo col d-flex sombra-baixo">
-            <h4 className='text-start text-info m-0 p-2 col align-self-center'>Logs</h4>
-            <MenuIcon updateTheme={updateTheme} />
-          </div>
-          <div className={`container-fluid m-0 p-0 vh-100 ${theme === "dark" ? "bg-dark" : "bg-light"}`}>
-            <DataTable
-              className=''
-              columns={columns}
-              striped
-              data={formattedData}
-              defaultSortField="id"
-              pagination
-              paginationServer
-              progressPending={loading}
-              paginationTotalRows={totalRows}
-              paginationPerPage={paginationPerPage}
-              paginationRowsPerPageOptions={[10, 20, 30]}
-              paginationComponentOptions={{
-                rowsPerPageText: 'Linhas por página:',
-                rangeSeparatorText: 'de',
-                noRowsPerPage: false,
-              }}
-              onChangePage={handlePageChange}
-              onChangeRowsPerPage={handlePerRowsChange}
-              text={customText}
-              highlightOnHover
-              pointerOnHover
-              theme={theme}
-              dense
-              subHeader
-              subHeaderComponent={
-                <div className='d-flex flex-row m-0 p-0 w-100  justify-content-between flex-wrap'>
-                  <div className='m-0 p-0 d-flex nowrap'>
-                    <OverlayTrigger placement='bottom' overlay={printButtonTooltipPDF}>
-                      <FontAwesomeIcon icon={faFilePdf} onClick={exportToPDF} className='btn btn-sm btn-light text-bg-primary p-1 m-1 fs-5' />
-                    </OverlayTrigger>
-                    <OverlayTrigger placement='bottom' overlay={printButtonTooltipExcel}>
-                      <FontAwesomeIcon icon={faFileExcel} className='btn btn-sm btn-light text-bg-primary p-1 m-1 fs-5' onClick={exportToExcel} />
-                    </OverlayTrigger>
-                  </div>
-                  <SearchField
-                    placeholder="Procurar"
-                    size="small"
-                    hasSearchButton={false}
-                    hasSearchIcon={false}
-                    labelHidden={false}
-                    onChange={(event) => onChange(event)}
-                    onClear={onClear}
-                    value={searchQuery}
-                    className='m-0 p-0 rounded border-0 text-center fw-bolder fs-6 input-group-sm flex-wrap d-flex'
-                    style={{
-                      textAlign: "center",
-                      borderRadius: "8px",
-                      lineHeight: "29px",
-                      width: '150px'
-                    }}
-                  />
-                </div>
-              }
-              subHeaderAlign="left"
-              style={{ width: '100%', height: '100%' }}
+      {
+        loading ? (
+          <div className='container-fluid text-center m-0 p-0 d-flex flex-column justify-content-center align-items-center align-self-center vh-100 vw-100 bg-fundo'>
+            <Triangle
+              color='cyan'
+              className='vh-100 vw-100 p-0 m-0'
+              ariaLabel="triangle-loading"
+              visible={true}
             />
+            <h1 className='text-center text-info mt-3 pt-3'>{customText.loading}</h1>
           </div>
-        </>
-      )}
+        ) : (
+          <>
+            <div className="top-0 text-end bg-fundo col d-flex sombra-baixo">
+              <h4 className='text-start text-info m-0 p-2 col align-self-center'>Logs</h4>
+              <MenuIcon updateTheme={updateTheme} className='p-0 m-0 bg-info text-info' />
+            </div>
+            <div className={`container-fluid m-0 p-0 vh-100 ${theme === "dark" ? "bg-dark" : "bg-light"}`}>
+              {showChart ? (
+                <Bar data={chartData} options={options} />
+              ) : (
+                <div className="d-none p-0 m-0"></div>
+              )}
+              <DataTable
+                className={`${showChart ? 'd-none' : 'd-flex'}`}
+                columns={columns}
+                striped
+                data={formattedData}
+                defaultSortField="id"
+                pagination
+                paginationServer
+                progressPending={loading}
+                paginationTotalRows={totalRows}
+                paginationPerPage={paginationPerPage}
+                paginationRowsPerPageOptions={[10, 20, 30]}
+                paginationComponentOptions={{
+                  rowsPerPageText: 'Linhas por página:',
+                  rangeSeparatorText: 'de',
+                  noRowsPerPage: false,
+                }}
+                onChangePage={handlePageChange}
+                onChangeRowsPerPage={handlePerRowsChange}
+                text={customText}
+                highlightOnHover
+                pointerOnHover
+                theme={theme}
+                dense
+                subHeader
+                subHeaderComponent={
+                  <div className='d-flex flex-row m-0 p-0 w-100  justify-content-between flex-wrap'>
+                    <div className='m-0 p-0 d-flex nowrap'>
+                      <OverlayTrigger placement='bottom' overlay={printButtonTooltipPDF}>
+                        <FontAwesomeIcon icon={faFilePdf} onClick={exportToPDF} className='btn btn-sm btn-light text-bg-primary p-1 m-1 fs-5' />
+                      </OverlayTrigger>
+                      <OverlayTrigger placement='bottom' overlay={printButtonTooltipExcel}>
+                        <FontAwesomeIcon icon={faFileExcel} className='btn btn-sm btn-light text-bg-primary p-1 m-1 fs-5' onClick={exportToExcel} />
+                      </OverlayTrigger>
+                      <OverlayTrigger placement='bottom' overlay={geraGraficoButtonTooltip}>
+                        <FontAwesomeIcon icon={faChartBar} className='btn btn-sm btn-light text-bg-primary p-1 m-1 fs-5' onClick={toggleChartVisibility} />
+                      </OverlayTrigger>
+                    </div>
+                    <SearchField
+                      placeholder="Procurar"
+                      size="small"
+                      hasSearchButton={false}
+                      hasSearchIcon={false}
+                      labelHidden={false}
+                      onChange={(event) => onChange(event)}
+                      onClear={onClear}
+                      value={searchQuery}
+                      className={`m-0 p-0 rounded border-0 text-center fw-bolder fs-6 input-group-sm flex-wrap ${showChart ? 'd-none' : 'd-flex'}`}
+                      style={{
+                        textAlign: "center",
+                        borderRadius: "8px",
+                        lineHeight: "29px",
+                        width: '150px'
+                      }}
+                    />
+                  </div>
+                }
+                subHeaderAlign="left"
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  display: showChart ? 'none' : 'flex',
+                }}
+
+              />
+            </div>
+          </>
+        )
+      }
     </>
   );
 };
