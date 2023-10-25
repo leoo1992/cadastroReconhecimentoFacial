d20111f795e55836df5ab2a8c0c0ddb3b982ce00
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-10-25 15:12:10 -0300

    refactor app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const sequelize = require("./config/sequelize");
 const cors = require("cors");
-const Pessoa = require("./models/Pessoa");
-const Users = require("./models/Users");
-const Log = require("./models/Log");
 const Associations = require("./models/associations");
 const app = express();
 const port = 3000;
-const { Op } = require('sequelize');
 const moment = require('moment-timezone');
 
 //IMPORTS USER
@@ -16,6 +12,7 @@ const cadastroUserRouter = require("./routes/cadastrosUsuario");
 const pesquisarUserRouter = require("./routes/pesquisarUser");
 const listarUserRouter = require("./routes/listarUser");
 const imprimirUserRouter = require("./routes/imprimirUser");
+const deletarUserByIdRouter = require("./routes/deletarUserById");
 
 //IMPORTS PESSOA
 const cadastroPessoaRouter = require("./routes/cadastroPessoa");
@@ -25,6 +22,12 @@ const consultaPessoaByIdRouter = require("./routes/consultaPessoaById");
 const pesquisaGeralPessoasRouter = require("./routes/pesquisaGeralPessoas");
 const listarPessoaRouter = require("./routes/listarPessoa");
 const imprimirPessoasRouter = require("./routes/imprimirPessoas");
+const deletarPessoaByIdRouter = require("./routes/deletarPessoaById");
+
+//IMPORTS LOG
+const pesquisarLogsRouter = require("./routes/pesquisarLogs");
+const listarLogsRouter = require("./routes/listarLogs");
+const imprimirLogsRouter = require("./routes/imprimirLogs");
 
 require("dotenv").config();
 app.use(express.json());
@@ -44,6 +47,7 @@ app.use("/", cadastroUserRouter);
 app.use("/", pesquisarUserRouter);
 app.use("/", listarUserRouter);
 app.use("/", imprimirUserRouter);
+app.use("/", deletarUserByIdRouter);
 
 //ROTAS PESSOA
 app.use("/", consultaPessoaByIdRouter);
@@ -53,147 +57,11 @@ app.use("/", pesquisaGeralPessoasRouter);
 app.use("/", desativarPessoaByIdRouter);
 app.use("/", listarPessoaRouter);
 app.use("/", imprimirPessoasRouter);
+app.use("/", deletarPessoaByIdRouter);
 
-app.get("/imprimiruser", async (req, res) => {
-  try {
-    const registros = await Users.findAll({
-      attributes: ['id', 'usuario'],
-    });
-    res.status(200).json({
-      registros,
-    });
-  } catch (err) {
-    console.error("Erro ao listar os dados: ", err);
-    res.status(500).json({ error: "Erro ao listar os dados." });
-  }
-});
-
-// rota listar Logs
-app.get("/listarlogs", async (req, res) => {
-  try {
-    const pagina = parseInt(req.query.pagina) || 1;
-    const limitePorPagina = parseInt(req.query.limitePorPagina) || 10;
-    const searchQuery = req.query.search || '';
-    let whereClause ={};
-    const totalRegistros = await Log.count({ where: whereClause });
-    const paginacao = (pagina - 1) * limitePorPagina;
-    const numeroDePaginas = Math.ceil(totalRegistros / limitePorPagina) || 1;
-
-    if (searchQuery) {
-      whereClause = {
-        ...whereClause,
-        [Op.or]: [
-          { 'id': { [Op.like]: `%${searchQuery}%` } },
-        ],
-      };
-    }
-    const registros = await Log.findAll({
-      where: whereClause,
-      limit: limitePorPagina,
-      offset: paginacao,
-      include: [
-        {
-          model: Pessoa,
-        },
-      ],
-    });
-    res.status(200).json({
-      registros,
-      numerodepaginas: numeroDePaginas,
-      totalregistros: totalRegistros,
-    });
-  } catch (err) {
-    console.error("Erro ao listar os dados: ", err);
-    res.status(500).json({ error: "Erro ao listar os dados." });
-  }
-});
-
-app.get("/imprimirlogs", async (req, res) => {
-  try {
-    const registros = await Log.findAll({
-      include: [
-        {
-          model: Pessoa,
-        },
-      ],
-    });
-    res.status(200).json({
-      registros,
-    });
-  } catch (err) {
-    console.error("Erro ao listar os dados: ", err);
-    res.status(500).json({ error: "Erro ao listar os dados." });
-  }
-});
-
-app.get("/pesquisarlogs", async (req, res) => {
-  try {
-    const { termo } = req.query;
-
-    if (!termo || termo.length < 3) {
-      return res.status(200).json({ resultados: [] });
-    }
-
-    console.log("Termo de pesquisa:", termo);
-
-    const registros = await Log.findAll({
-      where: {
-        [Op.or]: [
-          { '$Pessoas.nome$': { [Op.like]: `%${termo}%` } },
-        ],
-      },
-      include: [
-        {
-          model: Pessoa,
-          as: 'Pessoas',
-          through: {
-            attributes: [],
-          },
-        },
-      ],
-    });
-
-    res.status(200).json({ registros });
-  } catch (err) {
-    console.error("Erro ao realizar pesquisa geral: ", err);
-    res.status(500).json({ error: "Erro ao realizar pesquisa geral." });
-  }
-});
-
-//rota para deletar por id:
-app.delete("/deletar/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const pessoaExistente = await Pessoa.findByPk(id);
-
-    if (!pessoaExistente) {
-      return res.status(404).json({ error: "Registro não encontrado." });
-    }
-    await pessoaExistente.destroy();
-    res.status(204).send();
-  } catch (err) {
-    console.error("Erro ao excluir registro: ", err);
-    res.status(500).json({ error: "Erro ao excluir registro." });
-  }
-});
-
-// rota deletar usuario por id
-app.delete("/deletaruser/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const UserExistente = await Users.findByPk(id);
-
-    if (!UserExistente) {
-      return res.status(404).json({ error: "Registro não encontrado." });
-    }
-    await UserExistente.destroy();
-    res.status(204).send();
-  } catch (err) {
-    console.error("Erro ao excluir registro: ", err);
-    res.status(500).json({ error: "Erro ao excluir registro." });
-  }
-});
+//ROTAS LOG
+app.use("/", listarLogsRouter);
+app.use("/", imprimirLogsRouter);
+app.use("/", pesquisarLogsRouter);
 
 module.exports = app;
