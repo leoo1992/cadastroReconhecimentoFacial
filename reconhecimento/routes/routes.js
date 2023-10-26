0f6c34129478979376ecb4d5afc02ee0dce4ceb1
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-10-26 10:12:26 -0300

    adjust router init controller

--- reconhecimento/routes/routes.js
+++ reconhecimento/routes/routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pessoaController = require('../controllers/pessoaController');
 const logController = require('../controllers/logController');
+const initController = require('../controllers/initController');
 
-router.get('/obter-nomes', pessoaController.obterNomes);
+router.get('/obter-nomes', initController.obterNomes);
 router.post('/salvar-imagem', pessoaController.salvarImagem);
 router.post('/criar-pessoa', pessoaController.criarPessoa);
 router.post('/salvar-ou-atualizar-log', logController.salvarOuAtualizarLog);
