26f1cf981be6eac1cde3e23c117fb4e81b2e55a8
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2023-10-26 18:14:26 -0300

    theme global init development

---
+++ front-end/src/ThemeContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === "dark" ? "default" : "dark"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
